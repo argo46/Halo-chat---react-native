bf2572b8c9dd70ac5c3f27a6118972d31278ab52
argo46 <adityaargo4609@gmail.com>
2019-11-25 23:00:58 +0700

    rename app

--- android/app/src/main/java/com/mugglebyte/halochat/MainApplication.java
+++ android/app/src/main/java/com/mugglebyte/halochat/MainApplication.java
@@ -1,4 +1,4 @@
-package com.halochat;
+package com.mugglebyte.halochat;
 
 import android.app.Application;
 import android.content.Context;
--- ios/halochatTests/halochatTests.m
+++ ios/halochatTests/halochatTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface halochatTests : XCTestCase
+@interface HaloChatTests : XCTestCase
 
 @end
 
-@implementation halochatTests
+@implementation HaloChatTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
